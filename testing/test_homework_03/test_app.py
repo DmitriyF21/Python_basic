3b0ca0ae2c5c4d4a0cbf913aac10e6ddf6516be6
Дмитрий <dmitrijfefelov6@gmail.com>
2023-11-24 10:42:08 +0300

    commit : homework_03 first

--- testing/test_homework_03/test_app.py
+++ testing/test_homework_03/test_app.py
@@ -52,7 +52,7 @@ def run_image(docker_client, build_image):
     container: Container = docker_client.containers.run(build_image, detach=True, ports={PORT: LOCAL_PORT})
     print("running docker container detached")
     # give some time to for the web app to start
-    sleep(15)
+    sleep(3)
     yield container
     print("stopping docker container")
     container.stop()
