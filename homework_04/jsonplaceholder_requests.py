af1778fb42c114fdac2aba789aedf494175cd775
Дмитрий <dmitrijfefelov6@gmail.com>
2023-12-08 10:18:25 +0300

    commit : homework_04 first

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -13,10 +13,7 @@ async def fetch_json(url: str) -> dict:
         async with session.get(url) as response:
             data: dict = await response.json()
             return data
-#Внутри ф-и вызываем асинхронный менеджер, в нем открываем сессию через aiohttp, и следующим шагом
-# посылаем запрос GET по url session.get(url).
-# Результат кладем в переменную response и возвращаем его, преобразовав в json (return await response.json())
-# — код с return читается справа налево. Вначале await переменной, затем return того, что получилось)
+## для теста
 if __name__ == "__main__":
     asyncio.run(fetch_json(USERS_DATA_URL))
 
