77533d9cf46b9bfd3e00d2a90b1604dd8092f1a8
Дмитрий <dmitrijfefelov6@gmail.com>
2023-12-23 21:52:54 +0300

    commit : homework_04

--- homework_04/models.py
+++ homework_04/models.py
@@ -21,7 +21,7 @@ Base = declarative_base()
 
 
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
+Session = async_session
 
 class User(Base):
     __tablename__ = 'user'
